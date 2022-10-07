4139c75f88cb5f3adf6c1fd999f51c8006c730dd
jsb-mids <jujhar.bedi@gmail.com>
2022-10-07 10:07:49 -0700

    completing part 1

--- static/main.js
+++ static/main.js
@@ -1,14 +1,31 @@
- jQuery(function () {
-    $('.btn').click(function(event) {
-        jQuery.ajax({
-            url: '/',
-            data: {jsdata: event.currentTarget.name},
-            type: 'get',
-            success: function (response) {
-                jQuery("#myChart").attr("src", response)
-            }
-        });
-    });
-});
+var _iaq = _iaq || [];
+var click = 0;
 
+// Identify the user and set some fields on their Iterable profile 
+function identifyUser() {
+    _iaq.push(['identify', document.getElementById('email').value, {
+        "firstName": document.getElementById('firstName').value,
+        "isWebUser": true,
+        "SA_WebUser_Test_Key": "completed"
+    }]);
+}
 
+// Track a click event, passing a click count (from the current session)
+// that will be stored in the event's dataFields property
+function trackEvent() {
+    _iaq.push(['track', 'MyButtonClickedEvent', {
+        "clickNumber": ++click
+    }]);
+}
+
+// Replace <API key> with an Iterable API key (of type JavaScriptSDK)
+_iaq.push(['account', 'ff288e91e5f9442cb6ce6ea238886298']);
+
+(function () {
+    var b = document.createElement('script');
+    b.type = 'text/javascript';
+    b.async = true;
+    b.src = 'https://js.iterable.com/analytics.js';
+    var a = document.getElementsByTagName('script')[0];
+    a.parentNode.insertBefore(b, a);
+})();
